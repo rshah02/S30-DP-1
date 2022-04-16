7116475ed5a38324f50ea4000437f6a4c1a129d0
Rohan shah <rohan.kumarshah@outlook.com>
2022-04-16 16:04:16 -0700

    Create houseRobber.java

---
+++ houseRobber.java
@@ -0,0 +1,14 @@
+class Solution {
+    int skip;
+    public int rob(int[] nums) {        
+        if(nums==null || nums.length==0)return 0;
+        int n=nums.length;       
+        int take=nums[0];    
+        for(int i=1;i<n;i++){
+            int temp=take;
+           take=nums[i]+skip;
+            skip=Math.max(skip,temp);
+        }
+        return Math.max(skip,take);
+    }    
+  }
